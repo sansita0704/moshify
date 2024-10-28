44d917c2e8039f76614e89a70bcaea26f6bf097e
sansita0704 <sansita7406@gmail.com>
2024-10-28 22:43:01 +0530

    created a collapsible component

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+// Using this piece of code, we are selecting all the items having a class of collapsible.
+// Then, when the use clicks on it, we are inserting the collapsible--expanded class so that the collapsible gets expanded.
